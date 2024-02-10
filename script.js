4487930213e1c3a3208264c6a8d0c3b5a64e7afb
Prince Daksh <149432297+RudraCodeForge@users.noreply.github.com>
2024-02-11 03:29:17 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+var loader = document.querySelector('#loader');
+if (loader) {
+  setTimeout(function() {
+    loader.style.top = '-100%';
+  }, 5000);
+}
+
+
+//function for hamburger button 
+function menu() {
+  var mobileMenu = document.getElementsByClassName('mobilemenu')[0];
+  var links = document.getElementsByClassName('links')[0];
+  var hamburger = document.getElementsByClassName('hamburger')[0];
+
+  mobileMenu.classList.toggle('open');
+  links.classList.toggle('show');
+  hamburger.classList.toggle("change");
+}
+//Function to clear the form data
+function clearForm() {
+  document.getElementById("contact-form").reset();
+}
