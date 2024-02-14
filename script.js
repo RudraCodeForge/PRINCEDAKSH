a5c47133d8638f1af5179bf79244ed8042cca5cf
Prince Daksh <149432297+RudraCodeForge@users.noreply.github.com>
2024-02-14 20:59:06 +0530

    Update script.js
    Function for dark mode

--- script.js
+++ script.js
@@ -24,3 +24,19 @@ function BACK() {
    window.history.back();
 }
 
+// Function to toggle mode
+function toggleMode() {
+  const body = document.body;
+  const isDarkMode = !body.classList.contains('dark-mode');
+
+  body.classList.toggle('dark-mode', isDarkMode);
+  localStorage.setItem('darkMode', isDarkMode ? 'enabled' : 'disabled');
+}
+
+// Check for previously saved mode preference
+document.addEventListener('DOMContentLoaded', function() {
+  const darkModeSetting = localStorage.getItem('darkMode');
+  if (darkModeSetting === 'enabled') {
+    document.body.classList.add('dark-mode');
+  }
+});
